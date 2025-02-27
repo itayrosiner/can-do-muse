9336e3f5fef94979e327c5122a4e9a46a111a2c7
Avivtech <avivtech@gmail.com>
2025-02-28 01:25:22 +0200

    vanilla JS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.js"; // Ensure App.js exports a valid React component
+import "./index.css";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Root element not found");
 
-createRoot(rootElement).render(<App />);
+// Convert JSX <App /> to pure JavaScript using React.createElement
+createRoot(rootElement).render(React.createElement(App, null));
